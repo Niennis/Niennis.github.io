e8ed8469c7b82a7f7367a6f2a5fdadd65d16e2cd
Niennis <estefania.osses.v@gmail>
2018-03-04 23:01:03 -0300

    Agregados trabajos, habilidades y redes sociales

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  console.log('holi');
+});
+
+
+
+setTimeout(() => {
+  $('#estefania svg').after(`<h2 class="frontEnd">Front-End Developer</h2>`);
+  $('.containerAnimation').addClass('bgAnimation');
+}, 5000);
+
