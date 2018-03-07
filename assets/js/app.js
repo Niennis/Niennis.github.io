4a509e3a19dcd077fe6047dcf3e97f8451e8df62
Niennis <estefania.osses.v@gmail>
2018-03-07 11:09:34 -0300

    Intento envío mail de contacto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,33 @@
 $(document).ready(function() {
-  console.log('holi');
-$('#juju').fadeIn(2000);
+  $('#juju').fadeIn(2000);
   
-$('.myName').ready(function() {
-  console.log('front end');
-  setTimeout(() => {
-  $('#front').fadeIn(8000);
-  }, 1500);
-})
-
+  $('.myName').ready(function() {
+    console.log('front end');
+    setTimeout(() => {
+    $('#front').fadeIn(8000);
+    }, 1500);
+  })
 });
 
+$('#contactForm').submit(function(e) {
+  let name = $('#inputName');
+  let email = $('#email');
+  let message = $('#message');
+
+  if (!name.value || !email.value || !message.value) {
+    alertify.error('Por favor, revisa las entradas');
+  } else {
+    $.ajax({
+      url: 'https://formspree.io/estefania.osses.v@gmail.com',
+      method: 'POST',
+      data: $(this).serialize(),
+      dataType: 'json'
+    })
+    e.preventDefault();
+    $(this).get(0).reset();
+    alertify.success('Mensaje enviado')
+  }
+})
 
 // setTimeout(() => {
 //   $('#estefania svg').after(`<h2 id="front" class="frontEnd">Front-End Developer</h2>`);
