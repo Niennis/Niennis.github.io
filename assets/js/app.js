3cb96c9e22d29d1b6f6f9d53cb252710829fb7db
Niennis <estefania.osses.v@gmail>
2018-03-07 09:19:53 -0300

    Corrección en efectos de inicio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
   console.log('holi');
+$('#juju').fadeIn(2000);
+  
+$('.myName').ready(function() {
+  console.log('front end');
+  setTimeout(() => {
+  $('#front').fadeIn(8000);
+  }, 1500);
+})
+
 });
 
 
+// setTimeout(() => {
+//   $('#estefania svg').after(`<h2 id="front" class="frontEnd">Front-End Developer</h2>`);
+//   $('.containerAnimation').addClass('bgAnimation');
 
-setTimeout(() => {
-  $('#estefania svg').after(`<h2 class="frontEnd">Front-End Developer</h2>`);
-  $('.containerAnimation').addClass('bgAnimation');
-}, 5000);
+//   $('#estefania').fadeIn('slow')
+// }, 5000);
 
