6d4df328ff21167508c5ca091dc900c90e25b087
Niennis <estefania.osses.v@gmail.com>
2018-04-16 23:00:48 -0300

    Corrección en cv y fonts

--- assets/particles/app.js
+++ assets/particles/app.js
@@ -41,11 +41,11 @@ particlesJS('particles-js',
         }
       },
       "opacity": {
-        "value": 0.5,
+        "value": 0.7,
         "random": true,
         "anim": {
           "enable": false,
-          "speed": 1,
+          "speed": 4,
           "opacity_min": 0.1,
           "sync": false
         }
